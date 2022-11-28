8882f744e8b89525fbde7b99d2c7dbcef0b541ef
Jack Zhang <jack333zhang@gmail.com>
2022-11-28 23:40:14 +0800

    Part4.1-4.2

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const blogRouter = require('./controllers/blog')
+
+const url = config.MONGODB_URI
+logger.info('connecting to', url)
+mongoose.connect(url).then(() => {
+    logger.info('connected to MongoDB')
+}).catch(error => {
+    logger.error('error connecting to MongoDB:', error.message)
+})
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogRouter)
+module.exports = app
---
+++ models/blog.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+blogSchema.set('toJSON', {
+  transform: (document, returnObject) => {
+    returnObject.id = returnObject._id.toString()
+    delete returnObject._id
+    delete returnObject.__v
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
