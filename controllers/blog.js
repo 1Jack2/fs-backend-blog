5612500b28e7c6f804f996d7143df0099f545ec2
Jack Zhang <jack333zhang@gmail.com>
2022-12-01 22:05:52 +0800

    Eliminate the try-catch

--- controllers/blog.js
+++ controllers/blog.js
@@ -2,24 +2,16 @@ const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const {error} = require('../utils/logger')
 
-blogRouter.get('/', (request, response, next) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-    .catch(error => next(error))
+blogRouter.get('/', async (request, response, next) => {
+  const blogs = await Blog .find({})
+  response.json(blogs)
 })
 
-blogRouter.post('/', async (request, response, next) => {
+blogRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
-  try {
-    const savedBlog = await blog.save()
-    response.status(201).json(savedBlog)
-  } catch(exception) {
-    next(exception)
-  }
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
 
 module.exports = blogRouter
