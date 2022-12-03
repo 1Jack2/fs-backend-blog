3f7d17c1672a491f242f87810d1aaba7f69a5ae5
Jack Zhang <jack333zhang@gmail.com>
2022-12-03 21:09:50 +0800

    Part4.19

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,6 +1,17 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const logger = require('../utils/logger')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  logger.info('authorization =', authorization)
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogRouter.get('/', async (request, response, next) => {
   const blogs = await Blog.find({})
@@ -8,8 +19,14 @@ blogRouter.get('/', async (request, response, next) => {
 })
 
 blogRouter.post('/', async (request, response) => {
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!decodedToken.id) {
+    response.status(401).json({error: 'token missing or invalid'})
+  }
+
   const body = request.body
-  const user = await User.findById(body.userId)
+  const user = await User.findById(decodedToken.id)
   const blog = new Blog({
     title: body.title,
     author: body.author,
