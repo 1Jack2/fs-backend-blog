2cf210bb3ef01f690418f87fddcf9d7e3264f8b6
Jack Zhang <jack333zhang@gmail.com>
2022-12-03 21:42:05 +0800

    Part4.21

--- controllers/blog.js
+++ controllers/blog.js
@@ -34,6 +34,14 @@ blogRouter.post('/', async (request, response) => {
 })
 
 blogRouter.delete('/:id', async (request, response) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  const blog = await Blog.findById(request.params.id)
+  if (!blog) {
+    response.status(404).end()
+  }
+  if (blog.user.toString() !== decodedToken.id) {
+    response.status(401).json({error: 'permission denied'})
+  }
   await Blog.findByIdAndRemove(request.params.id)
   response.status(204).end()
 })
